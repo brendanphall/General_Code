24564d55bbbd3db08c4e7ecd0415ec9c701d1b44
Brendan Hall <73596893+brendanphall@users.noreply.github.com>
2025-04-25 11:38:32 -0400

    Script 5 working from .shp file

--- ATFS/5_calc_latlong/Test location.py
+++ ATFS/5_calc_latlong/Test location.py
@@ -1,4 +1,5 @@
 import os
+import arcpy
 
 shapefile_path = r"Z:\GitHub\General_Code\ATFS\5_calc_latlong\tmp_treefarm\dbo_treefarm.shp"
 
@@ -7,3 +8,13 @@ print("Path exists:", os.path.exists(shapefile_path))
 # Print directory contents for debugging
 print("Contents of directory:")
 print(os.listdir(r"Z:\GitHub\General_Code\ATFS\5_calc_latlong\tmp_treefarm"))
+
+# Print fields in the shapefile
+if os.path.exists(shapefile_path):
+    print("Fields in the shapefile:")
+    fields = arcpy.ListFields(shapefile_path)
+    for field in fields:
+        print(f"- {field.name} ({field.type})")
+else:
+    print("Shapefile does not exist, cannot list fields.")
+
